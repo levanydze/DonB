00e94f22fb140906585cb216e03a05fc38c52a60
levanydze <66974840+levanydze@users.noreply.github.com>
2024-04-16 16:23:04 +0200

    1

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,24 +1,24 @@
 import { companyDomain } from "../../controlFolder/control";
-import { fireData } from "./menu/functions";
 import { navItems } from "../../controlFolder/control";
+// import { fireData } from "./menu/functions";
 
 export default async function sitemap() {
-  const slug = await fireData();
+  // const slug = await fireData();
 
-  if (!slug) {
-    return null;
-  }
+  // if (!slug) {
+  // return null;
+  // }
 
   // Mapping menu items to their URL objects
-  const slugUrls = slug.flatMap((menus) =>
-    menus.menuItems.map((item) => ({
-      //MUST FIX
-      url: `${companyDomain}/menu/${item.id}`,
-      lastModified: new Date().toISOString(),
-      changeFrequency: "monthly",
-      priority: 0.7,
-    }))
-  );
+  // const slugUrls = slug.flatMap((menus) =>
+  //   menus.menuItems.map((item) => ({
+  //     //MUST FIX
+  //     url: `${companyDomain}/menu/${item.id}`,
+  //     lastModified: new Date().toISOString(),
+  //     changeFrequency: "monthly",
+  //     priority: 0.7,
+  //   }))
+  // );
 
   // Mapping nav items to their URL objects
   const navUrls = navItems.map((navItem) => ({
@@ -28,6 +28,7 @@ export default async function sitemap() {
     priority: navItem.homePage ? 1 : 0.6,
   }));
 
-  const allUrls = [...slugUrls, ...navUrls];
-  return allUrls;
+  // const allUrls = [...slugUrls, ...navUrls];
+
+  return navUrls;
 }
