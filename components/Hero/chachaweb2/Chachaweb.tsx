d8384748da267fb63ddfd9dd0670f3c2c5a224cc
levanydze <66974840+levanydze@users.noreply.github.com>
2024-04-25 17:40:12 +0200

    1

--- components/Hero/chachaweb2/Chachaweb.tsx
+++ components/Hero/chachaweb2/Chachaweb.tsx
@@ -1,16 +1,21 @@
-import Link from "next/link";
 import styles from "./Chachaweb.module.css";
-import Overlay from "./components/ImageComponents";
 import TextComponent from "./components/TextComponent";
 import ImageComponents from "./components/ImageComponents";
+import Image from "next/image";
+import imagesJson from "../../../json/images.json";
 
 export default function Chachaweb() {
+  const { webBuilding } = imagesJson;
   return (
     <div className="container4">
       <section>
         <div className={styles.chachawebHero2Wrapper}>
           <TextComponent />
           <ImageComponents />
+          <div className={styles.background}>
+            <div className={styles.overlay}></div>
+            <Image src={webBuilding} width={1000} height={1500} alt=""></Image>
+          </div>
         </div>
       </section>
     </div>
