5ca76c3b37d85ffba178f196dd29aeec50f39a69
levanydze <66974840+levanydze@users.noreply.github.com>
2024-05-20 13:00:25 +0200

    1

--- components/Accordions/Accordion1/Accordion.tsx
+++ components/Accordions/Accordion1/Accordion.tsx
@@ -15,28 +15,33 @@ export default function Accordion() {
   return (
     <div className="container1">
       <section>
-        <div className={styles.accordion}>
-          {accordionItems.map((item, index) => (
-            <div
-              className={`${styles.eachAccordion} ${
-                activeIndex === index ? styles.active : ""
-              }`}
-              key={index}
-              onClick={() => handleClick(index)}
-            >
-              <div className={styles.head}>
-                <h6 className="title3">{item.title}</h6>
-                <span> {activeIndex === index ? "-" : "+"} </span>
-              </div>
+        <div>
+          <h5 className={`title4 ${styles.headTitle}`}>
+            Feequntly Asked Questions
+          </h5>
+          <div className={styles.accordion}>
+            {accordionItems.map((item, index) => (
               <div
-                className={`${styles.content} ${
-                  activeIndex === index ? styles.show : styles.hide
+                className={`${styles.eachAccordion} ${
+                  activeIndex === index ? styles.active : ""
                 }`}
+                key={index}
+                onClick={() => handleClick(index)}
               >
-                <p className="text1">{item.content}</p>
+                <div className={styles.head}>
+                  <h6 className="title3 ">{item.title}</h6>
+                  <span> {activeIndex === index ? "-" : "+"} </span>
+                </div>
+                <div
+                  className={`${styles.content} ${
+                    activeIndex === index ? styles.show : styles.hide
+                  }`}
+                >
+                  <p className="text1">{item.content}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </section>
     </div>
