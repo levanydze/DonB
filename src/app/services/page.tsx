95478a24ac124ef2cc95775777f673b7f3e2f2ee
levanydze <66974840+levanydze@users.noreply.github.com>
2024-06-09 11:43:36 +0200

    1

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,24 +1,24 @@
-// import PageHeadImage from "../../../components/anyPageHead/PageHeadImage/PageHeadImage";
-// import imagesJson from "../../../json/images.json";
-// import ThreeReviews from "../../../components/sectionComponents/ThreeReviews/ThreeReviews";
-// import ServicesWrapper from "../../../components/WholeProjects/ChaChaWeb/ServicePage/ServicesWrapper";
-// import ItemsServicesArray from "./ItemsServicesArray/ItemsServicesArray";
-// import ContactForm from "../../../components/contactForms/CotactForm5/ContactForm";
-// import PageHeadSpace from "../../../components/anyPageHead/PageHadSpace/PageHeadSpace";
-// export const metadata = {
-//   title: "Services",
-// };
+import PageHeadImage from "../../../components/anyPageHead/PageHeadImage/PageHeadImage";
+import imagesJson from "../../../json/images.json";
+import ThreeReviews from "../../../components/sectionComponents/ThreeReviews/ThreeReviews";
+import ServicesWrapper from "../../../components/WholeProjects/ChaChaWeb/ServicePage/ServicesWrapper";
+import ItemsServicesArray from "./ItemsServicesArray/ItemsServicesArray";
+import ContactForm from "../../../components/contactForms/CotactForm5/ContactForm";
+import PageHeadSpace from "../../../components/anyPageHead/PageHadSpace/PageHeadSpace";
+export const metadata = {
+  title: "Services",
+};
 
-// export default function AboutPage() {
-//   const { roofDrone } = imagesJson;
-//   return (
-//     <div className="fadeOut">
-//       {/* <PageHeadSpace />
-//       <ItemsServicesArray
-//         title="Our Services"
-//         span="Services You Can Order"
-//         arrayNumber={99}
-//       /> */}
-//     </div>
-//   );
-// }
+export default function AboutPage() {
+  const { roofDrone } = imagesJson;
+  return (
+    <div className="fadeOut">
+      {/* <PageHeadSpace />
+      <ItemsServicesArray
+        title="Our Services"
+        span="Services You Can Order"
+        arrayNumber={99}
+      /> */}
+    </div>
+  );
+}
