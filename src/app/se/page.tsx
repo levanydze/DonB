96eb75baba2a32b6c8a5bbe77b6885f226c1b654
levanydze <66974840+levanydze@users.noreply.github.com>
2024-06-09 12:50:00 +0200

    1

--- src/app/se/page.tsx
+++ src/app/se/page.tsx
@@ -24,11 +24,13 @@ import Accordion from "../../../components/Accordions/Accordion1/Accordion";
 export default function Home() {
   return (
     <main className="fadeOut">
+      {/* <Header /> */}
+
       <Hero
         overlay
-        mainTitle="Svenska here"
         button1Target="/se/contact"
         button1Value="Börja nu"
+        mainTitle="Sovloft. WIC och platbyggda garderober. Skräddarsydda bokhyllor."
         text="Om du kan drömma det - kan vi bygga det!"
       />
       {/* <ItemsServicesArray
@@ -48,6 +50,8 @@ export default function Home() {
       <Accordion />
 
       <FourBox />*/}
+      {/* <Footer /> */}
+      {/* <LowerFoot /> */}
     </main>
   );
 }
