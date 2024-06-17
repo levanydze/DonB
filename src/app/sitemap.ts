54d4a26ad34c8ebee8ca87409ba4277aba3ce773
levanydze <66974840+levanydze@users.noreply.github.com>
2024-06-17 08:23:20 +0200

    1

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,11 +1,15 @@
 import { companyDomain } from "../../controlFolder/control";
 import { navItems } from "../../controlFolder/control";
 import { fetchData as fetchGalleryData } from "./en/gallery/DataFetch";
+import { fetchData as fetchGalleryDataSe } from "./se/gallery/DataFetch";
 import { fetchData as fetchServicesData } from "./en/services/DataFetch";
+import { fetchData as fetchServicesDataSe } from "./en/services/DataFetch";
 
 export default async function sitemap() {
   const galleryData = await fetchGalleryData();
   const servicesData = await fetchServicesData();
+  const galleryDataSe = await fetchGalleryDataSe();
+  const servicesDataSe = await fetchServicesDataSe();
 
   // if (!data || data.length === 0) {
   //   return null;
@@ -26,6 +30,20 @@ export default async function sitemap() {
     changeFrequency: "monthly",
     priority: 0.7,
   }));
+  const serviceSlugUrlsSe = servicesDataSe.map((item) => ({
+    url: `${companyDomain}/services/${item.id}`,
+    lastModified: new Date().toISOString(),
+    changeFrequency: "monthly",
+    priority: 0.7,
+  }));
+
+  // Mapping Gallery items to their URL objects
+  const gallerySlugUrlsSe = galleryDataSe.map((item) => ({
+    url: `${companyDomain}/gallery/${item.id}`,
+    lastModified: new Date().toISOString(),
+    changeFrequency: "monthly",
+    priority: 0.7,
+  }));
 
   // Mapping nav items to their URL objects
   const navUrls = navItems.map((navItem) => ({
@@ -36,7 +54,13 @@ export default async function sitemap() {
   }));
 
   // const allUrls = [...slugUrls, ...navUrls];
-  const allUrls = [...gallerySlugUrls, ...navUrls, ...serviceSlugUrls];
+  const allUrls = [
+    ...gallerySlugUrls,
+    ...serviceSlugUrls,
+    ...gallerySlugUrlsSe,
+    ...serviceSlugUrlsSe,
+    ...navUrls,
+  ];
 
   return allUrls;
 }
