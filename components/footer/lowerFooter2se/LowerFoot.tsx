8cd7db2563c4400ef5cac4f205acdac54e234890
levanydze <66974840+levanydze@users.noreply.github.com>
2024-08-22 16:44:19 +0200

    1

--- components/footer/lowerFooter2se/LowerFoot.tsx
+++ components/footer/lowerFooter2se/LowerFoot.tsx
@@ -9,9 +9,9 @@ export default function LowerFoot() {
     <div className="container2">
       <section>
         <div className={styles.lowerWrap}>
-          {/* <Link href={"https://levanidze.com"}> */}
-          {/* DESIGNED AND POWERED BY Levanidze.com */}
-          {/* </Link> */}
+          <Link href={"https://levanidze.com"}>
+            DESIGNED AND POWERED BY Levanidze.com
+          </Link>
           <p>Eftertryck förbjudes &copy; {companyFullName} </p>
         </div>
       </section>
